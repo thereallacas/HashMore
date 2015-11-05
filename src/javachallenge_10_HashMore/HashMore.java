57dc1bce2af46960b0a057d416fefcfafb1df9da
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2015-11-05 18:48:04 +0100

    skip vim

---
+++ src/javachallenge_10_HashMore/HashMore.java
@@ -0,0 +1,12 @@
+package javachallenge_10_HashMore;
+
+public class HashMore {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 100_000_000; i++) {
+			
+			args[0].hashCode();
+		}
+	}
+}
